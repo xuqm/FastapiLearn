b532386d06ac7565dffc57b896172df8f7539e02
xuqm <xuqinmin12@sina.com>
2020-05-15 09:48:38 +0800

    又写了一部分

---
+++ part6/main.py
@@ -0,0 +1,31 @@
+from fastapi import Path
+from pydantic import BaseModel
+
+from part6 import app
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
+    q: str = None,
+    item: Item = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
